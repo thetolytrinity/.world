77ab0a4f92c8ab2aa1e8a755a39585c9d69b80df
thetolytrinity <142269228+thetolytrinity@users.noreply.github.com>
2025-08-04 20:01:38 +0100

    favicon + preview

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,19 @@
 import restart from 'vite-plugin-restart'
 
 export default {
-    root: 'src/', // Sources files (typically where index.html is)
-    publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
-    server:
-    {
+    root: 'src/', // Source files 
+    publicDir: '../static/', // Path from "root" to static assets 
+    base: './', // Required for GitHub Pages to use relative paths
+    server: {
         host: true, // Open to local network and display URL
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if not in CodeSandbox
     },
-    build:
-    {
-        outDir: '../dist', // Output in the dist/ folder
+    build: {
+        outDir: '../docs', // Output into docs/ for GitHub Pages
         emptyOutDir: true, // Empty the folder first
         sourcemap: true // Add sourcemap
     },
-    plugins:
-    [
-        restart({ restart: [ '../static/**', ] }) // Restart server on static file change
-    ],
+    plugins: [
+        restart({ restart: [ '../static/**' ] }) // Restart server on static file change
+    ]
 }
